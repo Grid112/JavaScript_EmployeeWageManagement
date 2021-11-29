b76a5653af119c7e1ca2450b581f59566e5bfe51
Grid112 <sumitware1999@gmail.com>
2021-11-29 20:06:20 +0530

    Added: Ability To Check Employee's Attendance.

--- EmployeePayrollManagement.js
+++ EmployeePayrollManagement.js
@@ -1,1 +1,9 @@
-console.log("Welcome To Employee Payroll MAnagement Program Usin JAvaScript")
+
+const Is_Absent = 0;
+let empCheck = Math.floor(Math.random()*2);
+if (empCheck == Is_Absent){
+    console.log("Employee Is Absent");
+}
+else{
+    console.log("Employee Is Present");
+}
